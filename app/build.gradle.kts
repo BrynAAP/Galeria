f3b1aef3b49dede7e2f11b4545513b5ed1b9aaab
000_bryn <306660790+BrynAAP@users.noreply.github.com>
2026-08-19 18:48:01 +0000

    Sincronizando Java 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,15 @@ android {
         versionName = "1.0"
     }
 
+    // Estas son las líneas mágicas para que no peleen
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures {
         compose = true
     }
